5557206a5a5e8706ca78dc6eb76f19f4f4d92e3c
Amine Bekkali <amine.bekkali@protonmail.com>
2026-03-04 11:34:15 +0100

    Implementing scrim search message command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,9 @@
 import * as ping from "@/commands/ping";
 import * as scriminfo from "@/commands/scriminfo";
+import * as scrimsearchmsg from "@/commands/scrimsearchmsg";
 
 export const commands = [
     ping,
     scriminfo,
+    scrimsearchmsg,
 ];
---
+++ src/commands/scrimsearchmsg.ts
@@ -0,0 +1,48 @@
+import {
+    ChatInputCommandInteraction,
+    MessageFlags,
+    SlashCommandBuilder,
+    TextDisplayBuilder
+} from "discord.js";
+
+function createTextDisplay(date: string, time: string, format: string, elo: string) {
+    return new TextDisplayBuilder()
+        .setContent(`📅 **${date}**\n🕐 ${time} CET\n⚔️ ${format}\n✅ **${elo} Elo**\n✉️ DM + OP.GG`);
+}
+
+export async function execute(interaction: ChatInputCommandInteraction) {
+    await interaction.deferReply();
+
+    const date = interaction.options.get("date")?.value as string;
+    const time = interaction.options.get("time")?.value as string;
+    const format = interaction.options.get("format")?.value as string;
+    const elo = interaction.options.get("elo")?.value as string;
+
+    await interaction.followUp({
+        components: [createTextDisplay(date, time, format, elo)],
+        flags: MessageFlags.IsComponentsV2,
+    });
+}
+
+export const data = new SlashCommandBuilder()
+    .setName("scrim-search-msg")
+    .setDescription("Generates a message to search for scrims.")
+    .addStringOption(option => option.setName("date")
+        .setDescription("Scrim desired start date (i.e 03/03/2026)")
+        .setRequired(true))
+    .addStringOption(option => option.setName("time")
+        .setDescription("Scrim desired start time (i.e 20H or 2OH30)")
+        .setRequired(true))
+    .addStringOption(option => option.setName("format")
+        .setDescription("The format of the scrim to search for.")
+        .setRequired(true))
+    .addStringOption(option => option.setName("elo")
+        .setDescription("The desired Elo for the scrim.")
+        .addChoices(
+            { name: "Emerald-Diamond", value: "Emerald / Diamond" },
+            { name: "Diamond", value: "Diamond" },
+            { name: "Diamond-Master", value: "Diamond / Master" },
+            { name: "Low-Master", value: "Low Master" },
+            { name: "Master+", value: "Master+" },
+        )
+        .setRequired(true));
