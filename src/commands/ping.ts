553623d974f2fe91bfbbcf6bc8462492e62cbdf3
Amine Bekkali <amine.bekkali@protonmail.com>
2026-03-04 10:43:55 +0100

    Updating ping command file to be compliant with the command file structure

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,7 +1,12 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
-
-export const data = new SlashCommandBuilder().setName("ping").setDescription("Replies with Pong!");
+import {
+    CommandInteraction,
+    SlashCommandBuilder
+} from "discord.js";
 
 export async function execute(interaction: CommandInteraction) {
     return await interaction.reply("Pong!");
 }
+
+export const data = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!");
